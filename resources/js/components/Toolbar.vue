49f43fec31a5ede0dde55550465ecb196030af8a
RitonX <51285944+RitonX@users.noreply.github.com>
2020-02-22 23:30:01 +0330

    Vuetify and Vue Router
    VuetifyJs, Vue Router and Initial Componnets

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-app-bar
+            color="deep-purple accent-4"
+            dense
+            dark
+        >
+
+            <v-toolbar-title>RitonX</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn>Forum</v-btn>
+            <v-btn>Ask Question</v-btn>
+            <v-btn>Category</v-btn>
+            <v-btn>Login</v-btn>
+
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style>
+
+</style>
